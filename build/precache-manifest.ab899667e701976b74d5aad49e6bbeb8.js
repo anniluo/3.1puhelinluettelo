941c51270434ccb9cc5a361be9e7783c799ea013
anniluo <anniluo@metropolia.fi>
2019-07-08 19:43:47 +0300

    frontend build

---
+++ build/precache-manifest.ab899667e701976b74d5aad49e6bbeb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d67da6fa5fa99d40b3a468cb4e8e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3cc0692b76628045de",
+    "url": "/static/css/main.e9eb5e34.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "0f3cc0692b76628045de",
+    "url": "/static/js/main.b874a90c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
