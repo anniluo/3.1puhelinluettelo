5dc67dd035c464efddc9a51f8c54fc3deb798e0f
anniluo <anniluo@metropolia.fi>
2019-07-12 17:32:41 +0300

    3.17*: puhelinluettelo ja tietokanta, step4 (update an existing person)

--- index.js
+++ index.js
@@ -53,10 +53,11 @@ app.get('/info', (request, response) => {
     `)
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(people => {
         response.send(people.map(person => person.toJSON()))
     })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -71,11 +72,10 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -87,21 +87,29 @@ app.post('/api/persons', (request, response, next) => {
         })
     }
 
-  /*   if (Person.find({name: body.name})) {
-        return response.status(400).json({
-            error: 'name already added to the Phonebook'
-        })
-    } */
-
     const person = new Person({
         name: body.name,
         number: body.number
-    }) 
+    })
 
     person.save().then(savedPerson => {
         response.json(savedPerson.toJSON())
     })
     .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
